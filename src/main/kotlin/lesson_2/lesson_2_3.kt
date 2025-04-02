636e963490d020eb97dc2f690bd88f51076bdd9f
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-02 22:39:14 +0300

    lesson 2.3

---
+++ src/main/kotlin/lesson_2/lesson_2_3.kt
@@ -0,0 +1,19 @@
+package lesson_2
+
+fun main() {
+    var hour = 9
+    var minutes = 39
+    val fullTime = 457
+
+    var sHour = fullTime / 60
+    var sMin = fullTime - sHour * 60
+
+    hour += sHour
+    minutes += sMin
+
+    val newMin = hour * 60 + minutes
+    sHour = newMin / 60
+    sMin = newMin - sHour * 60
+
+    println("$sHour:$sMin")
+}
