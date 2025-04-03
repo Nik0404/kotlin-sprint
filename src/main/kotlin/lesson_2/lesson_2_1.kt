076fab16edfe16f2f50e63546f766a3394ee6ca4
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-03 21:37:05 +0300

    KS-2-1 new branch

--- src/main/kotlin/lesson_2/lesson_2_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_1.kt
@@ -1,12 +1,15 @@
 package lesson_2
 
+//new branch
 fun main() {
-    val student_1 = 3.0
-    val student_2 = 4.0
-    val student_3 = 3.0
-    val student_4 = 5.0
+    val studentOne = 3.0
+    val studentTwo = 4.0
+    val studentThree = 3.0
+    val studentFour = 5.0
 
-    val aref: Double =  (student_1 + student_2 + student_3 + student_4) / 4
+    val countStudent = 4
+
+    val aref: Double = (studentOne + studentTwo + studentThree + studentFour) / countStudent
 
     println(aref)
 }
