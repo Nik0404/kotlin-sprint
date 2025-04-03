160b13926ab63ed70a01abadc0848295517f0ed6
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-03 21:32:49 +0300

    KS-1-5 код улучшен

--- src/main/kotlin/lesson_1/lesson_1_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -2,10 +2,13 @@ package lesson_1
 
 fun main() {
     val seconds = 6480
+    val SIXTY = 60
 
-    val min =  (seconds / 60) - 60
+
+    val min = (seconds / SIXTY) - SIXTY
     val hour = seconds / 3600
+    val result = String.format("%02d:%02d:%02d", hour, min, 0)
 
-    println("0$hour:$min:00")
+    println(result)
 
 }
