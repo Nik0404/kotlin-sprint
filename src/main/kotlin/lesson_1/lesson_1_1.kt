75322085cd9bdd804236bdc8f4e3dec10d0c134f
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-03 20:35:06 +0300

    lesson 1.1 update

--- src/main/kotlin/lesson_1/lesson_1_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_1.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_1
 
 fun main() {
-    val messageShop: String = "спасибо"
-    var count: Int = 0
+    val messageShop: String = "спасибо за покупку"
+    var countOfOrders: Int
 }
