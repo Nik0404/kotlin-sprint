7ea1cb178c8fd29ca687caabd59d2fa156f40ec6
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-04 23:11:25 +0300

    KS-1-5 create new const

--- src/main/kotlin/lesson_1/lesson_1_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -1,12 +1,13 @@
 package lesson_1
 
+const val SIXTY = 60
+const val SECONDS_TO_HOUR = 3600
+
 fun main() {
     val seconds = 6480
-    val SIXTY = 60
-
 
     val min = (seconds / SIXTY) - SIXTY
-    val hour = seconds / 3600
+    val hour = seconds / SECONDS_TO_HOUR
     val result = String.format("%02d:%02d:%02d", hour, min, 0)
 
     println(result)
