4297e13a2e6fc3472658500ecef07cdd0ebb0610
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-05 15:16:53 +0300

    KS-3-1 new lesson

---
+++ src/main/kotlin/lesson_3/lesson_3_1.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val user = "lexa"
+    var greeting = "добрый день"
+
+    println("$greeting $user")
+
+    greeting = "добрый вечер"
+    println("$greeting $user")
+}
