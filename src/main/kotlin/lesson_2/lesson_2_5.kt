956aa6409a34a0e480e4e074d071f18152549888
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-05 14:40:33 +0300

    KS-2-5 add const

--- src/main/kotlin/lesson_2/lesson_2_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_5.kt
@@ -1,11 +1,13 @@
 package lesson_2
 
+const val ONE = 1
+
 fun main() {
     val contributionYear = 20.0
     val summ = 70000
     val bet = 0.167
 
-    val overBet = summ * Math.pow(1 + bet, contributionYear)
+    val overBet = summ * Math.pow(ONE + bet, contributionYear)
     val formatToResult = String.format("%.3f", overBet)
 
     println(formatToResult)
