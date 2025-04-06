b9f5cee26e2e5a3992890963ece7077e0168215d
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-06 10:47:57 +0300

    KS-2-4 update task

--- src/main/kotlin/lesson_2/lesson_2_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_4.kt
@@ -1,9 +1,13 @@
 package lesson_2
 
-const val boost = 0.2
+const val ONE_HUNDRED_PRESENT = 100
+
 fun main() {
+    var boost = 20
     val crystalOre = 7
     val ironOre = 11
+
+    boost = ONE_HUNDRED_PRESENT / boost
 
     val crystalOreToBaff = (crystalOre * boost).toInt()
     val ironOreToBaff = (ironOre * boost).toInt()
