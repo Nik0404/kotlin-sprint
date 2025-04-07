2e899ed6f1aab1f28e65825735912de981d8adde
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-07 22:17:09 +0300

    KS-3-4 update lesson

--- src/main/kotlin/lesson_3/lesson_3_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_4.kt
@@ -5,8 +5,7 @@ fun main() {
     var end = "E4"
     var strokeNumber = 1
 
-    val request = String.format("%s-%s;%s", start, end, strokeNumber)
-    println(request)
+    println("$start-$end;$strokeNumber")
 
     start = "D2"
     end = "D3"
