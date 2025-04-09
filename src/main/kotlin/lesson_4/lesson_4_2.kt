d00bf3e0e1ce051073ccdc8b4d4ac403c20dc472
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-09 21:28:02 +0300

    KS-4-2 update lesson

--- src/main/kotlin/lesson_4/lesson_4_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_2.kt
@@ -12,13 +12,13 @@ fun main() {
 
         println(
             "Груз с весом $oneCargoWeight кг и объемом $oneCargoVolume л соответствует категории 'Average': ${
-                (oneCargoWeight > FROM_WEIGHT && oneCargoWeight < UP_TO_WEIGHT) && oneCargoVolume <= VOLUME
+                (oneCargoWeight > FROM_WEIGHT && oneCargoWeight <= UP_TO_WEIGHT) && oneCargoVolume < VOLUME
             }"
         )
 
     println(
         "Груз с весом $twoCargoWeight кг и объемом $twoCargoVolume л соответствует категории 'Average': ${
-            (twoCargoWeight > FROM_WEIGHT && twoCargoWeight < UP_TO_WEIGHT) && twoCargoVolume <= VOLUME
+            (twoCargoWeight > FROM_WEIGHT && twoCargoWeight <= UP_TO_WEIGHT) && twoCargoVolume < VOLUME
         }"
     )
 }
