893d3ad5a024a4d6a40a93361f98cb36b2c1604a
nikita.dementev80 <nikita.dementev80@mail.ru>
2025-04-10 19:25:58 +0300

    KS-4-5 update lesson

--- src/main/kotlin/lesson_4/lesson_4_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_5.kt
@@ -1,16 +1,27 @@
 package lesson_4
 
+const val IS_SHIP_DAMAGE = true
+const val IS_WEATHER = true
+const val CREW_PERSONS_FIFTY = 50
+const val CREW_PERSONS_FIFTY_SEVENTY = 70
+const val COUNT_BOX_FIFTY = 50
+
 fun main() {
+    println("наличие повреждений корпуса? true / false")
     val isShipDamage = readln().toBoolean()
     val crewPersons = readln().toInt()
     val countBox = readln().toInt()
+    println("погода благоприятна или неблагоприятна? true / false")
     val isWeather = readln().toBoolean()
 
     println(
         "корабль может отплыть? ${
-            (isShipDamage == true && (crewPersons >= 50 && crewPersons <= 70) && (countBox > 50) &&
-                    (isWeather == true || isWeather == false)) ||
-                    (isShipDamage == false && crewPersons == 70 && isWeather == true && countBox >= 50)
+            (isShipDamage == IS_SHIP_DAMAGE && (crewPersons >= CREW_PERSONS_FIFTY && crewPersons <= CREW_PERSONS_FIFTY_SEVENTY) &&
+                    (countBox > COUNT_BOX_FIFTY) &&
+                    (isWeather == IS_WEATHER || isWeather == !IS_WEATHER)) ||
+                    (isShipDamage == !IS_SHIP_DAMAGE && crewPersons == CREW_PERSONS_FIFTY_SEVENTY &&
+                            isWeather == IS_WEATHER &&
+                            countBox >= CREW_PERSONS_FIFTY)
         }"
     )
 }
